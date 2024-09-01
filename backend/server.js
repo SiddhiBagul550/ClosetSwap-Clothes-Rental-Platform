d15e79f797d08417e4ccf4d9f73bd6118d659afb
SamarthBagde <bagdesamarth70@gmail.com>
2024-09-01 11:20:27 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,34 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
-// // app.use(express.json());
+app.use(express.json());
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+console.log(DB);
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.log("Database Error : " + err);
+  });
 
 app.get("/api", (req, res) => {
   res.send("server running!!");
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log("Server started at port : localhost:3000");
 });
