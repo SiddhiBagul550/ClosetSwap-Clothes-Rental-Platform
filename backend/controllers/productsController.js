3f0d74e9e04d262c535b3cc732fc1d5e7e5436ed
omkarbhanji <bhanjiomkar@gmail.com>
2024-09-22 23:12:20 +0530

    Delete and Patch updated

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,10 +1,10 @@
 const catchAsync = require("../utils/catchAsync");
-const product = require("./../models/productModel");
+const Product = require("./../models/productModel");
 const { promisify } = require("util");
 const user = require("./userController");
 
 exports.newproduct_entry = catchAsync(async (req, res) => {
-  const newproduct = await product.create({
+  const newproduct = await Product.create({
     name: req.body.name,
     img: req.body.img,
     category: req.body.category,
@@ -34,7 +34,7 @@ exports.getAllProducts = catchAsync(async (req, res, next) => {
 
   // console.log(category);
 
-  const products = await product.find({
+  const products = await Product.find({
     category: category,
     sub_category: sub_category,
   });
@@ -48,5 +48,46 @@ exports.getAllProducts = catchAsync(async (req, res, next) => {
     data: {
       products,
     },
+  });
+});
+
+exports.getProductsById = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+  console.log(id)
+  const product = await Product.findById(id);
+  res.status(200).json({
+    status: "success",
+    results: product.length,
+    data: {
+      product,
+    },
+  });
+
+});
+
+
+exports.deleteProduct = catchAsync(async (req, res, next) => {
+  
+  
+  const product = await Product.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: "Success",
+    data: null
+  });
+});
+
+exports.updateProduct = catchAsync(async (req, res, next) => {
+  
+  
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runvalidators: true
+
+  });
+  res.status(200).json({
+    status: "Success",
+    data: {
+      product,
+    }
   });
 });
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,27 +4,18 @@ const router = express.Router();
 
 const productsController = require("./../controllers/productsController");
 
-const getAllMenProducts = (req, res) => {
-  res.send("This is men product page");
-};
-
-const getAllWomenProducts = (req, res) => {
-  res.send("This is women product page");
-};
-
-const getAllKidsProducts = (req, res) => {
-  res.send("This is kids product page");
-};
 
 router
   .route("/:category/:sub_category")
   .get(productsController.getAllProducts)
   .post(productsController.newproduct_entry);
 
-router.route("/women/clothing").get(getAllWomenProducts);
+  router
+  .route("/:id")
+  .get(productsController.getProductsById)
+  .delete(productsController.deleteProduct)
+  .patch(productsController.updateProduct)
 
-router.route("/kids/clothing").get(getAllKidsProducts);
 
-// router.route('/:category/:cat')
 
 module.exports = router;
